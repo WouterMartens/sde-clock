b4e2ed485dda8000b1a22b5614f40ec55b5152ae
Wouter <woutermartens94@gmail.com>
2020-11-23 12:07:10 +0100

    Add timer, increment each second

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,28 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     private Console c;
     private Clock clock;
+    private Timer t;
 
     public static void main(String[] args) {
         // Create a new console to make logging easier
         Console c = new Console();
+        // Create a new timer to increment every second
+        Timer t = new Timer();
         // Create a new clock with a given time
         Clock clock = new Clock(23, 10, 40);
 
         System.out.println("Start clock");
 
-        /**
-         * Increments the clock for a given amount
-         * and writes the clock to the console
-         */
-        for (int i = 0; i < 61; i++) {
-            clock.count();
-            c.write(clock.toString());
-        }
+        // Increment the clock every second
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                clock.count();
+                c.write(clock.toString());
+            }
+        }, 0, 1000);
     }
 }
